681ab787cde9d104b51667720f62527fc3850916
Salman-Kb <137834927+Salman-Kb@users.noreply.github.com>
2023-11-12 07:22:27 +0400

    Get-User-Groups.ps1

--- Get-User-Groups.ps1
+++ Get-User-Groups.ps1
@@ -1,11 +1,11 @@
 #Get the groups for one AD account (We can export the result or not)
-Get-ADPrincipalGroupMembership salman.kabboul | Select-Object name, GroupCategory, GroupScope | Export-CSV `
-"C:\Users\Salman.kabboul\Desktop\Powershell\powershell-1.csv" –NoTypeInformation
+Get-ADPrincipalGroupMembership username | Select-Object name, GroupCategory, GroupScope | Export-CSV `
+"C:\Users\username\Desktop\Powershell\powershell-1.csv" –NoTypeInformation
 
 
 #Get the groups for multiple AD accounts imported from a file, export to a csv file (separated file for each user)
-$user = Get-Content "C:\Users\Salman.kabboul\Desktop\Powershell\List.txt"
+$user = Get-Content "C:\Users\username\Desktop\Powershell\List.txt"
 Foreach ($user in $user){
     Get-ADPrincipalGroupMembership $user | Select-Object name, GroupCategory, GroupScope | Export-CSV `
-     "C:\Users\Salman.kabboul\Desktop\Powershell\$user.csv" –NoTypeInformation
+     "C:\Users\username\Desktop\Powershell\$user.csv" –NoTypeInformation
 }
